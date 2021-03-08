a57e8506d875988576a230d0cb95655f30524f8d
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-03-08 13:58:34 -0500

    Add boilerplate to index.html, style.css, and script.js.

---
+++ script.js
@@ -0,0 +1,10 @@
+const BaseURL = 'https://www.dnd5eapi.co/api/';
+
+// D&D5e API Documentation: http://www.dnd5eapi.co/docs/
+
+// Call the API on page load
+// Call the API randomly on clicking generate
+// Let API populate fields with relevant information
+// Figure out how to let users download information as .txt document.
+// Add icing to the cake.
+// The cake is a lie...
