ffc699ac78543a518d2381c6b119c13d9e2d3bd6
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-03-09 17:23:56 -0500

    Setting up for art gallery.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const BaseURL = 'https://www.dnd5eapi.co/api/';
 
 // D&D5e API Documentation: http://www.dnd5eapi.co/docs/
 
-// Call the API on page load
+// Call the API on page load [DONE]
 // Call the API randomly on clicking generate
 // Let API populate fields with relevant information
 // Figure out how to let users download information as .txt document.
