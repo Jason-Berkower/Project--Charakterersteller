cf3a945add50fe3b7ee9a14e29ffe68d28c2a2a6
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-03-11 15:37:57 -0500

    Minor tweaks.

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const btn1 = document.querySelector('#generate');
 
 // D&D5e API Documentation: http://www.dnd5eapi.co/docs/
 
-// Call the API on page load [DONE]
-// Call the API randomly on clicking generate
-// Let API populate fields with relevant information
 // Figure out how to let users download information as .txt document.
 // Add icing to the cake.
 // The cake is a lie...
